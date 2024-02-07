d3c43568d24656d48a7d8d0e9ec4993e7d3c038a
denis <denispolyarush1405@gmail.com>
2024-02-08 02:13:52 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function Country(
+  title,
+  capital,
+  population = '1 million ',
+  area = '200 000km2'
+) {
+  this.title = title;
+  this.capital = capital;
+  this.population = population;
+  this.area = area;
+  this.showCountry = function () {
+    return `${title},  ${capital},  ${population},  ${area}`;
+  };
+}
+
+const USA = new Country('USA', 'Washington', '331.9 million', '9 834 000 km2');
+
+const Japan = new Country('Japan', 'Tokyo', '125.7 million', '377 973 km2');
+
+function funcCountry(countryObject) {
+  for (let key in countryObject) {
+    if (typeof countryObject[key] === 'string') {
+      console.log(`${key}: ${countryObject[key]}`);
+    }
+  }
+}
+funcCountry(USA)
